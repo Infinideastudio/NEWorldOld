d6719fad46dabcbbc71a32356dfd4b326407017e
Null <abc612008@126.com>
2015-10-18 11:27:19 +0800

    Optimization

--- source/World.h
+++ source/World.h
@@ -32,7 +32,7 @@ namespace world{
 	extern int unloadedChunks, unloadedChunksCount;
 	extern int chunkBuildRenderList[256][2];
 	extern int chunkLoadList[256][4];
-	extern int chunkUnloadList[256][4];
+	extern pair<chunk*, int> chunkUnloadList[256];
 	extern vector<unsigned int> vbuffersShouldDelete;
 	extern int chunkBuildRenders, chunkLoads, chunkUnloads;
 	extern bool* loadedChunkArray;
