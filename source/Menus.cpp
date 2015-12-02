b062347d8cef4c71caef95f1b28ac0c2db2b9b41
DREAMWORLDVOID <ys_37677@126.com>
2015-12-02 22:24:05 +0800

    GUI Improve Persave 2

--- source/Menus.cpp
+++ source/Menus.cpp
@@ -1,94 +1,21 @@
 #include "Menus.h"
-#include "World.h"
-#include "Textures.h"
-#include "TextRenderer.h"
-#include <shellapi.h>
-
 extern bool gamebegin;
 
-template<typename T>
-string strWithVar(string str, T var){
-	std::stringstream ss;
-	ss << str << var;
-	return ss.str();
-}
-int getDotCount(string s) {
-	int ret = 0;
-	for (unsigned int i = 0; i != s.size(); i++)
-		if (s[i] == '.') ret++;
-	return ret;
-}
+
 void MultiplayerGameMenu() {
-	MulliplayerGameMenu Menu=MulliplayerGameMenu();
-	gui::UIEnter((UIView*)&Menu);
+	NEMultiplayerGameMenu Menu = NEMultiplayerGameMenu();
+	gui::UIEnter((gui::UIView*)&Menu);
 }
 
 void mainmenu(){
-	//主菜单
-	
-	} while (!f);
-	MainForm.cleanup();
+	MainMenu Menu = MainMenu();
+	gui::UIEnter((gui::UIView*)&Menu);
 }
 
 void options(){
 	//设置菜单
-	gui::UIView MainForm;
-	int leftp = windowwidth / 2 - 250;
-	int rightp = windowwidth / 2 + 250;
-	int midp = windowwidth / 2;
-	int upp = 60;
-	int downp = windowheight - 20;
-	int lspc = 36;
-	bool f = false;
-	MainForm.Init();
-	TextRenderer::setFontColor(1.0, 1.0, 1.0, 1.0);
-	gui::UILabel		title = gui::UILabel("=================<  选 项  >=================");
-	gui::UITrackBar  FOVyBar = gui::UITrackBar(strWithVar("视野角度：", FOVyNormal), 120, (int)(FOVyNormal - 1));
-	gui::UITrackBar  mmsBar = gui::UITrackBar(strWithVar("鼠标灵敏度：", mousemove), 120, (int)(mousemove * 40 * 2 - 1));
-	gui::UITrackBar  viewdistBar = gui::UITrackBar(strWithVar("渲染距离：", viewdistance), 120, (viewdistance - 1) * 8 - 1);
-	//gui::UIButton*	ciArrayBtn = UIButton("使用区块索引数组：" + boolstr(UseCIArray))
-	gui::UIButton	rdstbtn = gui::UIButton(">> 渲染选项...");
-	gui::UIButton	gistbtn = gui::UIButton(">> 图形界面选项...");
-	gui::UIButton	backbtn = gui::UIButton("<< 返回主菜单");
-	//gui::UIButton*	savebtn = UIButton("保存设置")
-	//ciArrayBtn.enabled = false
-	//savebtn.enabled = false
-	do{
-		leftp = windowwidth / 2 - 250;
-		rightp = windowwidth / 2 + 250;
-		midp = windowwidth / 2;
-		downp = windowheight - 20;
-		title.UISetRect(midp - 225, midp + 225, 20, 36);
-		FOVyBar.UISetRect(leftp, midp - 10, upp + lspc * 0, upp + lspc * 0 + 24);
-		mmsBar.UISetRect(midp + 10, rightp, upp + lspc * 0, upp + lspc * 0 + 24);
-		viewdistBar.UISetRect(leftp, midp - 10, upp + lspc * 1, upp + lspc * 1 + 24);
-		//ciArrayBtn.UISetRect(midp + 10, rightp, upp + lspc * 1, upp + lspc * 1 + 24)
-		rdstbtn.UISetRect(leftp, midp - 10, upp + lspc * 4, upp + lspc * 4 + 24);
-		gistbtn.UISetRect(midp + 10, rightp, upp + lspc * 4, upp + lspc * 4 + 24);
-		backbtn.UISetRect(leftp, midp - 10, downp - 24, downp);
-		//savebtn.UISetRect(midp + 10, rightp, downp - 24, downp)
-		mb = glfwGetMouseButton(MainWindow, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS ? 1 : 0;
-		//更新GUI
-		glfwGetCursorPos(MainWindow, &mx, &my);
-		MainForm.mousedata((int)mx, (int)my, mw, mb);
-		MainForm.update();
-		FOVyNormal = static_cast<float>(FOVyBar.barpos + 1);
-		mousemove = (mmsBar.barpos / 2 + 1) / 40.0f;
-		viewdistance = viewdistBar.barpos / 8 + 2;
-		FOVyBar.text = strWithVar("视野角度：", FOVyNormal);
-		mmsBar.text = strWithVar("鼠标灵敏度：", mousemove);
-		viewdistBar.text = strWithVar("渲染距离：", viewdistance);
-		//ciArrayBtn.text = strWithVar("使用区块索引数组, ", boolstr(UseCIArray))
-		//if (ciArrayBtn.clicked) UseCIArray = !UseCIArray
-		if (rdstbtn.clicked) Renderoptions();
-		if (gistbtn.clicked) GUIoptions();
-		if (backbtn.clicked) f = true;
-		MainForm.render();
-		glfwSwapBuffers(MainWindow);
-		glfwPollEvents();
-		if (glfwWindowShouldClose(MainWindow)) exit(0);
-	} while (!f);
-	MainForm.cleanup();
+	Options Menu = Options();
+	gui::UIEnter((gui::UIView*)&Menu);
 }
 
 void Renderoptions(){
@@ -182,7 +109,7 @@ void worldmenu(){
 	MainForm.Init();
 	TextRenderer::setFontColor(1.0, 1.0, 1.0, 1.0);
 	gui::UILabel title = gui::UILabel("==============<  选 择 世 界  >==============");
-	gui::UIVerticalScroll gui::UIVerticalScroll = UIVerticalScroll(100, 0);
+	gui::UIVerticalScroll UIVerticalScroll = UIVerticalScroll(100, 0);
 	gui::UIButton enterbtn = gui::UIButton("进入选定的世界");
 	gui::UIButton deletebtn = gui::UIButton("删除选定的世界");
 	gui::UIButton backbtn = gui::UIButton("<< 返回主菜单");
