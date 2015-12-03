77bb76052041a406733f3a72e7be4d83c3c9beb3
DREAMWORLDVOID <ys_37677@126.com>
2015-12-03 19:45:24 +0800

    Revert "Revert "Revert "Revert "GUI Improve Persave 1""""
    This reverts commit 1dc25afce6ff9dede8da3d8f09ae5eba3f864971.

--- source/Menus.h
+++ source/Menus.h
@@ -2,15 +2,12 @@
 #include "Definitions.h"
 #include "GUI.h"
 <<<<<<< HEAD
-<<<<<<< HEAD
 #include "Menus.h"
 #include "World.h"
 #include "Textures.h"
 #include "TextRenderer.h"
 #include <shellapi.h>
 
-=======
->>>>>>> parent of 7e2d023... GUI Improve Persave 1
 =======
 >>>>>>> parent of 7e2d023... GUI Improve Persave 1
 void mainmenu();
