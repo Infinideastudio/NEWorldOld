88a0c98948f08d9e989dc39a43c822b2abd3a9bb
SuperSodaSea <bobby825@126.com>
2015-12-11 23:06:52 +0800

    Updata AABB

--- source/Hitbox.h
+++ source/Hitbox.h
@@ -16,18 +16,18 @@ namespace Hitbox
 	extern AABB Emptybox;
 	extern bool stuck;
 
-	bool inXclip(AABB boxA, AABB boxB);
-	bool inYclip(AABB boxA, AABB boxB);
-	bool inZclip(AABB boxA, AABB boxB);
+	bool inXclip(AABB& boxA, AABB& boxB);
+	bool inYclip(AABB& boxA, AABB& boxB);
+	bool inZclip(AABB& boxA, AABB& boxB);
 
-	bool Hit(AABB boxA, AABB boxB);
+	bool Hit(AABB& boxA, AABB& boxB);
 
-	double MaxMoveOnXclip(AABB boxA, AABB boxB, double movedist);
-	double MaxMoveOnYclip(AABB boxA, AABB boxB, double movedist);
-	double MaxMoveOnZclip(AABB boxA, AABB boxB, double movedist);
+	double MaxMoveOnXclip(AABB& boxA, AABB& boxB, double movedist);
+	double MaxMoveOnYclip(AABB& boxA, AABB& boxB, double movedist);
+	double MaxMoveOnZclip(AABB& boxA, AABB& boxB, double movedist);
 
-	AABB Expand(AABB box, double xe, double ye, double ze);
+	AABB Expand(AABB& box, double xe, double ye, double ze);
 	void Move(AABB &box, double xa, double ya, double za);
 	void MoveTo(AABB &box, double x, double y, double z);
-	void renderAABB(AABB box, float colR, float colG, float colB, int mode);
+	void renderAABB(AABB& box, float colR, float colG, float colB, int mode);
 }
