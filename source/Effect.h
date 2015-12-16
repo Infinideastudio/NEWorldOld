175b91a0913b4ec78e4e3af2fc9dc19317b4f02d
SuperSodaSea <bobby825@126.com>
2015-12-16 22:36:15 +0800

    Add Effect

---
+++ source/Effect.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+namespace Effect {
+
+	void readScreen(int x, int y, int w, int h, uint8_t* data);
+	void writeScreen(int x, int y, int w, int h, uint8_t* data);
+
+	void gray(int w, int h, uint8_t* src, uint8_t* dst);
+	void blurGaussian(int w, int h, uint8_t* src, uint8_t* dst, int r);
+
+}
