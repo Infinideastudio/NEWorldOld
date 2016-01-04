270394b8d2422b76da688d7a93f21be16d3794ea
Bruce Dai <zgnjycxxdyt@sina.com>
2016-01-04 22:31:31 +0800

    Manually merge

--- source/Hitbox.h
+++ source/Hitbox.h
@@ -17,19 +17,19 @@ namespace Hitbox
 	extern AABB Emptybox;
 	extern bool stuck;
 
-	bool inXclip(AABB& boxA, AABB& boxB);
-	bool inYclip(AABB& boxA, AABB& boxB);
-	bool inZclip(AABB& boxA, AABB& boxB);
+	bool inXclip(const AABB& boxA, const AABB& boxB);
+	bool inYclip(const AABB& boxA, const AABB& boxB);
+	bool inZclip(const AABB& boxA, const AABB& boxB);
 
-	bool Hit(AABB& boxA, AABB& boxB);
+	bool Hit(const AABB& boxA, const AABB& boxB);
 
-	double MaxMoveOnXclip(AABB& boxA, AABB& boxB, double movedist);
-	double MaxMoveOnYclip(AABB& boxA, AABB& boxB, double movedist);
-	double MaxMoveOnZclip(AABB& boxA, AABB& boxB, double movedist);
+	double MaxMoveOnXclip(const AABB& boxA, const AABB& boxB, double movedist);
+	double MaxMoveOnYclip(const AABB& boxA, const AABB& boxB, double movedist);
+	double MaxMoveOnZclip(const AABB& boxA, const AABB& boxB, double movedist);
 
-	AABB Expand(AABB& box, double xe, double ye, double ze);
+	AABB Expand(const AABB& box, double xe, double ye, double ze);
 	void Move(AABB &box, double xa, double ya, double za);
 	void MoveTo(AABB &box, double x, double y, double z);
-	void renderAABB(AABB& box, float colR, float colG, float colB, int mode);
+	void renderAABB(const AABB& box, float colR, float colG, float colB, int mode);
 }
 #endif
