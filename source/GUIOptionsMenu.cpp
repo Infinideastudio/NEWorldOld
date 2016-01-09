231b4795f59333acd9467b6b9ff0607447011961
qiaozhanrong <qiaozhanrong@163.com>
2016-01-09 16:08:47 +0800

    Arrange code

--- source/GUIOptionsMenu.cpp
+++ source/GUIOptionsMenu.cpp
@@ -7,18 +7,18 @@ namespace Menus {
 		GUI::label title;
 		GUI::button fontbtn, blurbtn, backbtn;
 		void onLoad() {
-			title = GUI::label("===============< " + GetStrbyKey("gui:caption:GUI Options") + " >==============", -225, 225, 20, 36, 0.5, 0.5, 0.0, 0.0);
-			fontbtn = GUI::button(GetStrbyKey("gui:All Unicode Font") + "£º", -250, -10, 60, 84, 0.5, 0.5, 0.0, 0.0);
-			blurbtn = GUI::button(GetStrbyKey("gui:BackGround Blur") + "£º", 10, 250, 60, 84, 0.5, 0.5, 0.0, 0.0);
-			backbtn = GUI::button("<< " + GetStrbyKey("gui:Back to Option Menu"), -250, 250, -44, -20, 0.5, 0.5, 1.0, 1.0);
+			title = GUI::label(GetStrbyKey("gui.gui.caption"), -225, 225, 20, 36, 0.5, 0.5, 0.0, 0.0);
+			fontbtn = GUI::button("", -250, -10, 60, 84, 0.5, 0.5, 0.0, 0.0);
+			blurbtn = GUI::button("", 10, 250, 60, 84, 0.5, 0.5, 0.0, 0.0);
+			backbtn = GUI::button(GetStrbyKey("gui.gui.back"), -250, 250, -44, -20, 0.5, 0.5, 1.0, 1.0);
 			registerControls(4, &title, &fontbtn, &blurbtn, &backbtn);
 		}
 		void onUpdate() {
 			if (fontbtn.clicked) TextRenderer::useUnicodeASCIIFont = !TextRenderer::useUnicodeASCIIFont;
 			if (blurbtn.clicked) GUIScreenBlur = !GUIScreenBlur;
 			if (backbtn.clicked) ExitSignal = true;
-			fontbtn.text = GetStrbyKey("gui:All Unicode Font") + "£º" + BoolYesNo(TextRenderer::useUnicodeASCIIFont);
-			blurbtn.text = GetStrbyKey("gui:BackGround Blur") + "£º" + BoolEnabled(GUIScreenBlur);
+			fontbtn.text = GetStrbyKey("gui.gui.unicode") + BoolYesNo(TextRenderer::useUnicodeASCIIFont);
+			blurbtn.text = GetStrbyKey("gui.gui.blur") + BoolEnabled(GUIScreenBlur);
 		}
 	};
 	void GUIoptions() { GUIOptionsMenu Menu; Menu.start(); }
--- source/RenderOptionsMenu.cpp
+++ source/RenderOptionsMenu.cpp
@@ -8,13 +8,13 @@ namespace Menus {
 		GUI::button smoothlightingbtn, fancygrassbtn, mergefacebtn, backbtn;
 		GUI::trackbar msaabar;
 		void onLoad() {
-			title = GUI::label("==============< " + GetStrbyKey("gui:caption:Render Options") + "  >==============", -225, 225, 20, 36, 0.5, 0.5, 0.0, 0.0);
-			smoothlightingbtn = GUI::button(GetStrbyKey("gui:Smooth Lighting") + "：", -250, -10, 60, 84, 0.5, 0.5, 0.0, 0.0);
-			fancygrassbtn = GUI::button(GetStrbyKey("gui:Grass Tex Gluing") + "：", 10, 250, 60, 84, 0.5, 0.5, 0.0, 0.0);
-			mergefacebtn = GUI::button(GetStrbyKey("gui:Linar Face-Merging") + "：", -250, -10, 96, 120, 0.5, 0.5, 0.0, 0.0);
-			msaabar = GUI::trackbar("多重采样抗锯齿：", 120, Multisample == 0 ? 0 : (int)(log2(Multisample) - 1) * 30 - 1, 10, 250, 96, 120, 0.5, 0.5, 0.0, 0.0);
-			backbtn = GUI::button("<< " + GetStrbyKey("gui:Back to Option Menu"), -250, 250, -44, -20, 0.5, 0.5, 1.0, 1.0);
-			registerControls(6, &title, &smoothlightingbtn, &fancygrassbtn, &mergefacebtn, &backbtn);
+			title = GUI::label(GetStrbyKey("gui.render.caption"), -225, 225, 20, 36, 0.5, 0.5, 0.0, 0.0);
+			smoothlightingbtn = GUI::button("", -250, -10, 60, 84, 0.5, 0.5, 0.0, 0.0);
+			fancygrassbtn = GUI::button("", 10, 250, 60, 84, 0.5, 0.5, 0.0, 0.0);
+			mergefacebtn = GUI::button("", -250, -10, 96, 120, 0.5, 0.5, 0.0, 0.0);
+			msaabar = GUI::trackbar("", 120, Multisample == 0 ? 0 : (int)(log2(Multisample) - 1) * 30 - 1, 10, 250, 96, 120, 0.5, 0.5, 0.0, 0.0);
+			backbtn = GUI::button(GetStrbyKey("gui.render.back"), -250, 250, -44, -20, 0.5, 0.5, 1.0, 1.0);
+			registerControls(6, &title, &smoothlightingbtn, &fancygrassbtn, &mergefacebtn, &msaabar, &backbtn);
 			if (MergeFace) SmoothLighting = smoothlightingbtn.enabled = NiceGrass = fancygrassbtn.enabled = false;
 		}
 		void onUpdate() {
@@ -29,10 +29,10 @@ namespace Menus {
 			else Multisample = 1 << ((msaabar.barpos + 1) / 30 + 1);
 			if (backbtn.clicked) ExitSignal = true;
 			std::stringstream ss; ss << Multisample;
-			msaabar.text = "多重采样抗锯齿：" + (Multisample != 0 ? ss.str() + "x" : BoolEnabled(false));
-			smoothlightingbtn.text = GetStrbyKey("gui:Smooth Lighting") + "：" + BoolEnabled(SmoothLighting);
-			fancygrassbtn.text = GetStrbyKey("gui:Grass Tex Gluing") + "：" + BoolEnabled(NiceGrass);
-			mergefacebtn.text = GetStrbyKey("gui:Linar Face-Merging") + "：" + BoolEnabled(MergeFace);
+			smoothlightingbtn.text = GetStrbyKey("gui.render.smooth") + BoolEnabled(SmoothLighting);
+			fancygrassbtn.text = GetStrbyKey("gui.render.grasstex") + BoolEnabled(NiceGrass);
+			mergefacebtn.text = GetStrbyKey("gui.render.merge") + BoolEnabled(MergeFace);
+			msaabar.text = GetStrbyKey("gui.render.multisample") + (Multisample != 0 ? ss.str() + "x" : BoolEnabled(false));
 		}
 	};
 	void Renderoptions() { RenderOptionsMenu Menu; Menu.start(); }
