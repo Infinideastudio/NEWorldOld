e828deacc1ccf15c711fb22f60df35bfbe804f12
qiaozhanrong <qiaozhanrong@163.com>
2016-01-09 16:52:47 +0800

    Change language key prefix
    - Change language key prefix to 'NEWorld.'

--- source/MainMenu.cpp
+++ source/MainMenu.cpp
@@ -8,9 +8,9 @@ namespace Menus {
 		GUI::button startbtn, optionsbtn, quitbtn;
 		void onLoad() {
 			title = GUI::imagebox(0.0f, 1.0f, 0.5f, 1.0f, tex_title, -256, 256, 20, 276, 0.5, 0.5, 0.0, 0.0);
-			startbtn = GUI::button(GetStrbyKey("gui.main.start"), -200, 200, 280, 312, 0.5, 0.5, 0.0, 0.0);
-			optionsbtn = GUI::button(GetStrbyKey("gui.main.options"), -200, -3, 318, 352, 0.5, 0.5, 0.0, 0.0);
-			quitbtn = GUI::button(GetStrbyKey("gui.main.exit"), 3, 200, 318, 352, 0.5, 0.5, 0.0, 0.0);
+			startbtn = GUI::button(GetStrbyKey("NEWorld.main.start"), -200, 200, 280, 312, 0.5, 0.5, 0.0, 0.0);
+			optionsbtn = GUI::button(GetStrbyKey("NEWorld.main.options"), -200, -3, 318, 352, 0.5, 0.5, 0.0, 0.0);
+			quitbtn = GUI::button(GetStrbyKey("NEWorld.main.exit"), 3, 200, 318, 352, 0.5, 0.5, 0.0, 0.0);
 			registerControls(4, &title, &startbtn, &optionsbtn, &quitbtn);
 		}
 		void onUpdate() {
