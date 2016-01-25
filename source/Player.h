0281db522111f0eda5832af07c544a5d88f1a42d
qiaozhanrong <qiaozhanrong@163.com>
2016-01-25 11:37:31 +0800

    Improve inventory
    - Player::addItem() will find the grid with the same block first!

--- source/Player.h
+++ source/Player.h
@@ -19,7 +19,7 @@ public:
 	static bool save(string worldn);
 	static bool load(string worldn);
 
-	static void addItem(item itemname, short amount = 1);
+	static bool addItem(item itemname, short amount = 1);
 	static bool putBlock(int x, int y, int z, block blockname);
 
 	static PlayerPacket convertToPlayerPacket();
