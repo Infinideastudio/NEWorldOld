40ceb6ed5cc82cea108f9bcac7ef6cacec21a697
Null <abc612008@126.com>
2016-02-01 16:13:00 +0800

    Game mode

--- source/NEWorld.cpp
+++ source/NEWorld.cpp
@@ -23,23 +23,15 @@
 #include "Globalization.h"
 #include "Command.h"
 #include "ModLoader.h"
+#include "Setup.h"
 
-void WindowSizeFunc(GLFWwindow* win, int width, int height);
-void MouseButtonFunc(GLFWwindow*, int button, int action, int);
-void CharInputFunc(GLFWwindow*, unsigned int c);
-void MouseScrollFunc(GLFWwindow*, double, double yoffset);
-void splashscreen();
-void createwindow();
-void setupscreen();
-void setupNormalFog();
-void LoadTextures();
 bool loadGame();
 void saveGame();
 ThreadFunc updateThreadFunc(void*);
 void drawCloud(double px, double pz);
 void updategame();
 void debugText(string s, bool init = false);
-void Render();
+void render();
 void drawBorder(int x,int y,int z);
 void renderDestroy(float level,int x,int y,int z);
 void drawGUI();
@@ -47,8 +39,8 @@ void drawBagRow(int row, int itemid, int xbase, int ybase, int spac, float alpha
 void drawBag();
 void saveScreenshot(int x, int y, int w, int h, string filename);
 void createThumbnail();
-void loadoptions();
-void saveoptions();
+void loadOptions();
+void saveOptions();
 int getMouseScroll() { return mw; }
 int getMouseButton() { return mb; }
 void registerCommands();
@@ -93,7 +85,7 @@ int main() {
 	setlocale(LC_ALL, "zh_CN.UTF-8");
 #endif
 
-	loadoptions();
+	loadOptions();
 	Globalization::Load();
 	
 	_mkdir("Configs");
@@ -104,11 +96,11 @@ int main() {
 	windowwidth = defaultwindowwidth;
 	windowheight = defaultwindowheight;
 	cout << "[Console][Event]Initialize GLFW" << (glfwInit() == 1 ? "" : " - Failed!") << endl;
-	createwindow();
-	setupscreen();
+	createWindow();
+	setupScreen();
 	glDisable(GL_CULL_FACE);
-	splashscreen();
-	LoadTextures();
+	splashScreen();
+	loadTextures();
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glMatrixMode(GL_MODELVIEW);
@@ -177,7 +169,7 @@ main_menu:
 			upsc = 0;
 		}
 
-		Render();
+		render();
 
 		if (glfwGetKey(MainWindow, GLFW_KEY_ESCAPE) == 1) {
 			updateThreadPaused = true;
@@ -286,158 +278,6 @@ ThreadFunc updateThreadFunc(void*){
 	return 0;
 }
 
-void WindowSizeFunc(GLFWwindow* win, int width, int height) {
-	if (width<640) width = 640;
-	if (height<360) height = 360;
-	windowwidth = width;
-	windowheight = height > 0 ? height : 1;
-	glfwSetWindowSize(win, width, height);
-	setupscreen();
-}
-
-void MouseButtonFunc(GLFWwindow*, int button, int action, int){
-	mb = 0;
-	if (action == GLFW_PRESS){
-		if (button == GLFW_MOUSE_BUTTON_LEFT)mb += 1;
-		if (button == GLFW_MOUSE_BUTTON_RIGHT)mb += 2;
-		if (button == GLFW_MOUSE_BUTTON_MIDDLE)mb += 4;
-	}
-	else mb = 0;
-}
-
-void CharInputFunc(GLFWwindow*, unsigned int c) {
-	if (c >= 128) {
-		wchar_t* pwszUnicode = new wchar_t[2];
-		pwszUnicode[0] = (wchar_t)c;
-		pwszUnicode[1] = '\0';
-		char* pszMultiByte;
-		pszMultiByte = (char*)malloc((unsigned int)4);
-		pszMultiByte = (char*)realloc(pszMultiByte, WCharToMByte(pszMultiByte, pwszUnicode, 4));
-		inputstr += pszMultiByte;
-		free(pszMultiByte);
-		delete[] pwszUnicode;
-	}
-	else inputstr += (char)c;
-}
-
-void MouseScrollFunc(GLFWwindow*, double, double yoffset) {
-	mw += (int)yoffset;
-}
-
-void splashscreen(){
-	TextureID splTex = Textures::LoadRGBTexture("Textures/GUI/splashscreen.bmp");
-	glEnable(GL_TEXTURE_2D);
-	for (int i = 0; i < 256; i += 2) {
-		glfwSwapBuffers(MainWindow);
-		glfwPollEvents();
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glBindTexture(GL_TEXTURE_2D, splTex);
-		glColor4f((float)i / 256, (float)i / 256, (float)i / 256, 1.0);
-		glBegin(GL_QUADS);
-			glTexCoord2f(0.0, 1.0); glVertex2i(-1, 1);
-			glTexCoord2f(850.0f / 1024.0f, 1.0); glVertex2i(1, 1);
-			glTexCoord2f(850.0f / 1024.0f, 1.0 - 480.0f / 1024.0f); glVertex2i(1, -1);
-			glTexCoord2f(0.0, 1.0 - 480.0f / 1024.0f); glVertex2i(-1, -1);
-		glEnd();
-		Sleep(10);
-	}
-	glfwSwapBuffers(MainWindow);
-	glfwPollEvents();
-}
-
-void createwindow() {
-	glfwSetErrorCallback([](int, const char* desc) { cout << desc << endl; });
-	std::stringstream title;
-	title << "NEWorld " << MAJOR_VERSION << MINOR_VERSION << EXT_VERSION;
-	if (Multisample != 0) glfwWindowHint(GLFW_SAMPLES, Multisample);
-	MainWindow = glfwCreateWindow(windowwidth, windowheight, title.str().c_str(), NULL, NULL);
-	MouseCursor = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
-	glfwMakeContextCurrent(MainWindow);
-	glfwSetCursor(MainWindow, MouseCursor);
-	glfwSetInputMode(MainWindow, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-	glfwSetWindowSizeCallback(MainWindow, &WindowSizeFunc);
-	glfwSetMouseButtonCallback(MainWindow, &MouseButtonFunc);
-	glfwSetScrollCallback(MainWindow, &MouseScrollFunc);
-	glfwSetCharCallback(MainWindow, &CharInputFunc);
-}
-
-void setupscreen() {
-
-	//获取OpenGL版本
-	GLVersionMajor = glfwGetWindowAttrib(MainWindow, GLFW_CONTEXT_VERSION_MAJOR);
-	GLVersionMinor = glfwGetWindowAttrib(MainWindow, GLFW_CONTEXT_VERSION_MINOR);
-	GLVersionRev = glfwGetWindowAttrib(MainWindow, GLFW_CONTEXT_REVISION);
-	//获取OpenGL函数地址
-	InitGLProc();
-
-	//渲染参数设置
-	glViewport(0, 0, windowwidth, windowheight);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glShadeModel(GL_SMOOTH);
-	glDisable(GL_DITHER);
-	glEnable(GL_CULL_FACE);
-	glEnable(GL_TEXTURE_2D);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_ALPHA_TEST);
-	glEnable(GL_BLEND);
-	glEnable(GL_LINE_SMOOTH);
-	glDepthFunc(GL_LEQUAL);
-	glAlphaFunc(GL_GREATER, 0.0); //<--这家伙在卖萌？(往后面看看，卖萌的多着呢)
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
-	glHint(GL_FOG_HINT, GL_FASTEST);
-	glHint(GL_LINE_SMOOTH_HINT, GL_FASTEST);
-	if (Multisample != 0) glEnable(GL_MULTISAMPLE_ARB);
-	glPixelStorei(GL_PACK_ALIGNMENT, 4);
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-	glColor4f(0.0, 0.0, 0.0, 1.0);
-	TextRenderer::BuildFont(windowwidth, windowheight);
-	TextRenderer::setFontColor(1.0, 1.0, 1.0, 1.0);
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glClearDepth(1.0);
-	glGenBuffersARB(1, &World::EmptyBuffer);
-	if (Renderer::AdvancedRender) Renderer::initShaders();
-	if (vsync) glfwSwapInterval(1);
-	else glfwSwapInterval(0);
-}
-
-void setupNormalFog() {
-	float fogColor[4] = { skycolorR, skycolorG, skycolorB, 1.0f };
-	glEnable(GL_FOG);
-	glFogi(GL_FOG_MODE, GL_LINEAR);
-	glFogfv(GL_FOG_COLOR, fogColor);
-	glFogf(GL_FOG_START, viewdistance * 16.0f - 32.0f);
-	glFogf(GL_FOG_END, viewdistance * 16.0f);
-}
-
-void LoadTextures(){
-	//载入纹理
-	Textures::Init();
-	
-	tex_select = Textures::LoadRGBATexture("Textures/GUI/select.bmp", "");
-	tex_unselect = Textures::LoadRGBATexture("Textures/GUI/unselect.bmp", "");
-	tex_title = Textures::LoadRGBATexture("Textures/GUI/title.bmp", "Textures/GUI/titlemask.bmp");
-	for (int i = 0; i < 6; i++) {
-		std::stringstream ss;
-		ss << "Textures/GUI/mainmenu" << i << ".bmp";
-		tex_mainmenu[i] = Textures::LoadRGBTexture(ss.str());
-	}
-	
-	DefaultSkin = Textures::LoadRGBATexture("Textures/Player/skin_xiaoqiao.bmp", "Textures/Player/skinmask_xiaoqiao.bmp");
-
-	for (int gloop = 1; gloop <= 10; gloop++){
-		string path = "Textures/blocks/destroy_" + itos(gloop) + ".bmp";
-		DestroyImage[gloop] = Textures::LoadRGBATexture(path, path);
-	}
-
-	BlockTextures = Textures::LoadRGBATexture("Textures/blocks/Terrain.bmp", "Textures/blocks/Terrainmask.bmp");
-	BlockTextures3D = Textures::LoadBlock3DTexture("Textures/blocks/Terrain3D.bmp", "Textures/blocks/Terrain3Dmask.bmp");
-	loadItemsTextures();
-}
-
 void saveGame(){
 	World::saveAllChunks();
 	if (!Player::save(World::worldname)) {
@@ -518,6 +358,12 @@ void registerCommands() {
 		World::explode(x, y, z, r);
 		return true;
 	}));
+	commands.push_back(Command("/gamemode", [](const vector<string>& command) {
+		if (command.size() != 2) return false;
+		int mode; conv(command[1], mode);
+		Player::changeGameMode(mode);
+		return true;
+	}));
 }
 
 bool doCommand(const vector<string>& command) {
@@ -537,11 +383,11 @@ void updategame(){
 	
 	Player::BlockInHand = Player::inventory[3][Player::indexInHand];
 	//生命值相关
-	if (Player::health > 0) {
+	if (Player::health > 0 || Player::gamemode==Player::Creative) {
 		if (Player::health < Player::healthMax) Player::health += Player::healSpeed;
 		if (Player::health > Player::healthMax) Player::health = Player::healthMax;
 	}
-	else Player::health = 1;
+	else Player::spawn();
 
 	//World::unloadedChunks=0
 	World::rebuiltChunks = 0;
@@ -850,8 +696,8 @@ void updategame(){
 
 			//各种设置切换
 			if (isPressed(GLFW_KEY_F1)) {
-				Player::Flying = !Player::Flying;
-				Player::jump = 0.0;
+				Player::changeGameMode(Player::gamemode == Player::Creative ?
+					Player::Survival : Player::Creative);
 			}
 			if (isPressed(GLFW_KEY_F2)) shouldGetScreenshot = true;
 			if (isPressed(GLFW_KEY_F3)) DebugMode = !DebugMode;
@@ -870,7 +716,7 @@ void updategame(){
 				DebugMergeFace = !DebugMergeFace;
 				DebugMode = true;
 			}
-			if (isPressed(GLFW_KEY_F4)) Player::CrossWall = !Player::CrossWall;
+			if (isPressed(GLFW_KEY_F4 && Player::gamemode == Player::Creative)) Player::CrossWall = !Player::CrossWall;
 			if (isPressed(GLFW_KEY_F5)) GUIrenderswitch = !GUIrenderswitch;
 			if (isPressed(GLFW_KEY_F6)) Player::Glide = !Player::Glide;
 			if (isPressed(GLFW_KEY_F7)) Player::spawn();
@@ -1013,7 +859,7 @@ void debugText(string s, bool init) {
 	pos++;
 }
 
-void Render() {
+void render() {
 	//画场景
 	double curtime = timer();
 	double TimeDelta;
@@ -1525,22 +1371,24 @@ void drawGUI(){
 
 	glDisable(GL_CULL_FACE);
 
-	glColor4d(0.8, 0.0, 0.0, 0.3);
-	glBegin(GL_QUADS);
-	glVertex2d(10, 10);
-	glVertex2d(200, 10);
-	glVertex2d(200, 30);
-	glVertex2d(10, 30);
-	glEnd();
+	if (Player::gamemode == Player::Survival) {
+		glColor4d(0.8, 0.0, 0.0, 0.3);
+		glBegin(GL_QUADS);
+		glVertex2d(10, 10);
+		glVertex2d(200, 10);
+		glVertex2d(200, 30);
+		glVertex2d(10, 30);
+		glEnd();
 
-	double healthPercent = (double)Player::health / Player::healthMax;
-	glColor4d(1.0, 0.0, 0.0, 0.5);
-	glBegin(GL_QUADS);
-	glVertex2d(20, 15);
-	glVertex2d(20 + healthPercent * 170, 15);
-	glVertex2d(20 + healthPercent * 170, 25);
-	glVertex2d(20, 25);
-	glEnd();
+		double healthPercent = (double)Player::health / Player::healthMax;
+		glColor4d(1.0, 0.0, 0.0, 0.5);
+		glBegin(GL_QUADS);
+		glVertex2d(20, 15);
+		glVertex2d(20 + healthPercent * 170, 15);
+		glVertex2d(20 + healthPercent * 170, 25);
+		glVertex2d(20, 25);
+		glEnd();
+	}
 
 	TextRenderer::setFontColor(1.0f, 1.0f, 1.0f, 0.9f);
 	if (chatmode) {
@@ -1967,7 +1815,7 @@ void loadoption(std::map<string, string> &m, const char* name, T &value) {
 	ss << m[name]; ss >> value;
 }
 
-void loadoptions() {
+void loadOptions() {
 	std::map<string, string> options;
 	std::ifstream filein("Configs/options.ini", std::ios::in);
 	if (!filein.is_open()) return;
@@ -1999,7 +1847,7 @@ void saveoption(std::ofstream &out, const char* name, T &value) {
 	out << string(name) << " " << value << endl;
 }
 
-void saveoptions() {
+void saveOptions() {
 	std::map<string, string> options;
 	std::ofstream fileout("Configs/options.ini", std::ios::out);
 	if (!fileout.is_open()) return;
