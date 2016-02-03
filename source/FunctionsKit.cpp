98fce35b57f89a5c3644a098f5e322e709a8b706
Null <abc612008@126.com>
2016-02-03 12:19:23 +0800

    重构

---
+++ source/FunctionsKit.cpp
@@ -0,0 +1,18 @@
+#include "FunctionsKit.h"
+unsigned int g_seed;
+
+vector<string> split(string str, string pattern) {
+	vector<string> ret;
+	if (pattern.empty()) return ret;
+	size_t start = 0, index = str.find_first_of(pattern, 0);
+	while (index != str.npos)
+	{
+		if (start != index)
+			ret.push_back(str.substr(start, index - start));
+		start = index + 1;
+		index = str.find_first_of(pattern, start);
+	}
+	if (!str.substr(start).empty())
+		ret.push_back(str.substr(start));
+	return ret;
+}
