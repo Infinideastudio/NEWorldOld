bdcc37d9ccaa2534e8280c95ea3d559d6d4030c2
Null <abc612008@126.com>
2016-02-03 12:19:23 +0800

    重构

--- source/Definitions.cpp
+++ source/Definitions.cpp
@@ -71,7 +71,6 @@ int mw, mb, mbp, mbl, mwl;
 double mxdelta, mydelta;
 //键盘输入数据
 string inputstr;
-unsigned int g_seed;
 
 #ifdef NEWORLD_DEBUG_PERFORMANCE_REC
 int c_getChunkPtrFromCPA;
@@ -79,66 +78,3 @@ int c_getChunkPtrFromSearch;
 int c_getHeightFromHMap;
 int c_getHeightFromWorldGen;
 #endif
-
-#ifdef NEWORLD_USE_WINAPI
-unsigned int MByteToWChar(wchar_t* dst, const char* src, unsigned int n){
-	int res = MultiByteToWideChar(CP_ACP, 0, src, n, dst, n);
-	return res;
-}
-unsigned int WCharToMByte(char* dst, const wchar_t* src, unsigned int n){
-	return WideCharToMultiByte(CP_ACP, 0, src, n, dst, n * 2, NULL, NULL);
-}
-#else
-void Sleep(unsigned int ms){
-	unsigned int fr = clock();
-	while (clock() - fr <= ms);
-	return;
-}
-#endif
-
-void DebugWarning(string msg){
-#ifdef NEWORLD_USE_WINAPI
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-	printf("[Debug][Warning]");
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-	printf("%s\n", msg.c_str());
-#else
-	printf("[Debug][Warning]%s\n", msg.c_str());
-#endif
-}
-
-void DebugError(string msg){
-#ifdef NEWORLD_USE_WINAPI
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
-	printf("[Debug][Error]");
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-	printf("%s\n", msg.c_str());
-#else
-	printf("[Debug][Error]%s\n", msg.c_str());
-#endif
-}
-
-//常用函数
-vector<string> split(string str, string pattern)
-{
-	vector<string> ret;
-	if (pattern.empty()) return ret;
-	size_t start = 0, index = str.find_first_of(pattern, 0);
-	while (index != str.npos)
-	{
-		if (start != index)
-			ret.push_back(str.substr(start, index - start));
-		start = index + 1;
-		index = str.find_first_of(pattern, start);
-	}
-	if (!str.substr(start).empty())
-		ret.push_back(str.substr(start));
-	return ret;
-}
-
-void UITrans(int x, int y) {
-	glTranslated((static_cast<double>(x))*stretch, (static_cast<double>(y))*stretch, 0);
-}
-void UIVertex(int x, int y) {
-	glVertex2i((static_cast<double>(x))*stretch, (static_cast<double>(y))*stretch);
-}
