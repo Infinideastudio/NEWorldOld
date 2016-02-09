7b50faa6a02cb4dcc7fca458a0ebce944738572d
DREAMWORLDVOID <yshliu0321@icloud.com>
2016-02-09 15:39:18 +0800

    Fix bug and refix bug #34

--- source/GameMenu.cpp
+++ source/GameMenu.cpp
@@ -18,11 +18,11 @@ namespace Menus {
 			//Make update thread realize that it should pause
 			MutexLock(Mutex);
 			if (resumebtn.clicked) ExitSignal = true;
-			if (exitbtn.clicked) gameexit = ExitSignal = true;
-		}
-		void onLeave() {
-			World::saveAllChunks();
-			World::destroyAllChunks();
+			if (exitbtn.clicked) {
+				gameexit = ExitSignal = true;
+				World::saveAllChunks();
+				World::destroyAllChunks();
+			}
 		}
 	};
 	void gamemenu() { GameMenu Menu; Menu.start(); }
