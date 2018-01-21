8b91d59d430e2dd9ebed14566362a94e42a68c67
qiaozhanrong <qiaozhanrong@163.com>
2018-01-21 17:45:18 +0800

    恢复作死地形

--- source/WorldGen.cpp
+++ source/WorldGen.cpp
@@ -32,10 +32,10 @@ namespace WorldGen {
         fractional_X = x - int_X;
         int_Y = (int)floor(y);
         fractional_Y = y - int_Y;
-        v1 = SmoothedNoise(int_X, int_Y);
-        v2 = SmoothedNoise(int_X + 1, int_Y);
-        v3 = SmoothedNoise(int_X, int_Y + 1);
-        v4 = SmoothedNoise(int_X + 1, int_Y + 1);
+        v1 = Noise(int_X, int_Y);
+        v2 = Noise(int_X + 1, int_Y);
+        v3 = Noise(int_X, int_Y + 1);
+        v4 = Noise(int_X + 1, int_Y + 1);
         i1 = Interpolate(v1, v2, fractional_X);
         i2 = Interpolate(v3, v4, fractional_X);
         return Interpolate(i1, i2, fractional_Y);
