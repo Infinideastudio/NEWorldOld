bb980d026cc4c84aa0aaf812eac5a2deda990f56
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2025-05-02 11:57:04 +0100

    misc: support cmake only

--- cmake/CppcheckCodeAnalysis.cmake
+++ cmake/CppcheckCodeAnalysis.cmake
@@ -4,21 +4,21 @@ string(TOUPPER ${PROJECT_NAME} PREFIX)
 option(${PREFIX}_ENABLE_CODE_ANALYSIS "Run code analysis with cppcheck" OFF)
 
 # See: https://stackoverflow.com/questions/56957172/cmake-conditionally-run-cppcheck
-if(${PREFIX}_ENABLE_CODE_ANALYSIS)
-  find_program(cppcheck cppcheck)
-  if(cppcheck MATCHES "NOTFOUND")
-    message(STATUS "Cppcheck executable not found.")
-  else()
-    message(STATUS "Cppcheck executable found at ${cppcheck}.")
-    # Enable cppcheck.
-    set(CMAKE_CXX_CPPCHECK "${cppcheck}"
-      "--enable=all"
-      "--inconclusive"
-      "--inline-suppr"
-      "--quiet"
-      "--suppress=unmatchedSuppression"
-      "--suppress=unusedFunction"
-      "--template='{file}:{line}: warning: {id} ({severity}): {message}'"
-    )
-  endif()
-endif()
+if (${PREFIX}_ENABLE_CODE_ANALYSIS)
+    find_program(cppcheck cppcheck)
+    if (cppcheck MATCHES "NOTFOUND")
+        message(STATUS "Cppcheck executable not found.")
+    else ()
+        message(STATUS "Cppcheck executable found at ${cppcheck}.")
+        # Enable cppcheck.
+        set(CMAKE_CXX_CPPCHECK "${cppcheck}"
+            "--enable=all"
+            "--inconclusive"
+            "--inline-suppr"
+            "--quiet"
+            "--suppress=unmatchedSuppression"
+            "--suppress=unusedFunction"
+            "--template='{file}:{line}: warning: {id} ({severity}): {message}'"
+        )
+    endif ()
+endif ()
